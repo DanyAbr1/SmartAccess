6fd68838a7a75467be89aaf91ffdea5093d6ea53
Dany <dannyb1392@gmail.com>
2020-12-11 14:17:37 -0400

    User and Context

---
+++ SmartAccess/SmartAccess.Api/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartAccess.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartAccess.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly SmartAccessContext _context;
+
+        public UserController(SmartAccessContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<User>> Get(string nombre, string pass)
+        {
+            var usuario = await _context.Users.FirstOrDefaultAsync(x => x.UserName == nombre && x.Password == pass);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return usuario;
+        }
+    }
+}
---
+++ SmartAccess/SmartAccess.Api/Models/SmartAccessContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartAccess.Api.Models
+{
+    public partial class SmartAccessContext:DbContext
+    {
+        public SmartAccessContext(DbContextOptions<SmartAccessContext> options)
+            :base(options)
+        {
+
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
